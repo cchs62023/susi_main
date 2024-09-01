d43a934c5e8817ad7a8bd1ae4c1afaa999132a69
linda <monthttest970509@gmail.com>
2024-09-01 11:31:33 +0800

    Initial Commit

---
+++ susi_main/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  susi_main
+//
+//  Created by 林家齊 on 2024/9/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
